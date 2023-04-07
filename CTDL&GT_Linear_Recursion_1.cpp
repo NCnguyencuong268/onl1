fe31759183736bfe4e701f1d2b3ab7137b8eb1c4
NCnguyencuong268 <125277756+NCnguyencuong268@users.noreply.github.com>
2023-04-07 11:22:24 +0700

    Add files via upload

---
+++ CTDL&GT_Linear_Recursion_1.cpp
@@ -0,0 +1,11 @@
+//Nguon tham khao: https://freetuts.net/de-quy-tuyen-tinh-2946.html
+
+#include <iostream>
+using namespace std;
+
+ 
+int factorial(int n){
+  if(n == 0) 
+  return 1; 
+  return n * factorial(n-1);
+}
---
+++ CTDL&GT_Linear_Recursion_2.cpp
@@ -0,0 +1,20 @@
+//Nguon tham khao: https://freetuts.net/de-quy-tuyen-tinh-2946.html
+
+#include <iostream>
+using namespace std;
+
+ 
+int factorial(int n){
+  if(n == 0) 
+  return 1; 
+  return n * factorial(n-1);
+}
+
+int main() {
+  int n;
+  cout<<"Nhap giai thua: ";
+  cin>>n;
+  int kq = factorial(n);
+  cout<<"\nKet qua \n"<<n<<"! = "<<kq;
+   
+}
