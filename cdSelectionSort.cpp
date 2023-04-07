dd5d1ab1f659289f5042a13484c5ce9d33201b0e
NCnguyencuong268 <125277756+NCnguyencuong268@users.noreply.github.com>
2023-04-07 09:41:18 +0700

    Add files via upload

---
+++ cdSelectionSort.cpp
@@ -0,0 +1,16 @@
+// Nguon tham khao: https://gochocit.com/ky-thuat-lap-trinh/thuat-toan-sap-xep-chon-truc-tiep-selection-sort
+
+#include <iostream>
+using namespace std;
+void Selection_Sort(int a[], int n){
+	int min;
+	for(int i=0; i<n-1; i++){
+		min = i;
+		for(int j=i+1; j<n; j++){
+			if (a[j] < a[min]){
+				min = j;
+			}
+		}
+		swap(a[min], a[i]);
+	}
+}
