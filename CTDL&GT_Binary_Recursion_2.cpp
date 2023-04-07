a4ffd026bf8cb71c48c5f1e74b93d6f1202f8b39
NCnguyencuong268 <125277756+NCnguyencuong268@users.noreply.github.com>
2023-04-07 11:58:34 +0700

    Add files via upload

---
+++ CTDL&GT_Binary_Recursion_2.cpp
@@ -0,0 +1,18 @@
+//Nguon tham khao: https://freetuts.net/de-quy-nhi-phan-binary-recursion-2953.html
+
+#include <iostream>
+using namespace std;
+int fib(int n){
+  if(n <= 2) 
+  return 1;
+  return fib(n - 1) + fib(n - 2);
+}
+ 
+int main() {
+  int kq,n;
+  cout<<"Nhap vi tri muon tim trong day fibonacci: ";
+  cin>>n;
+  kq = fib(n);
+  cout<<"\nSo fibonacci o vi tri "<<n<<" là: "<<kq;
+ 
+}
